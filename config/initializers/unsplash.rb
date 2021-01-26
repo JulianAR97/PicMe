7667d40f3143c7305dd00d3b628f119416efcc9f
Julian <julianrosenthal@yahoo.com>
2021-01-26 15:42:42 -0700

    added unsplash initializer

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,6 @@
+Unsplash.configure do |config|
+  config.application_access_key = "00AMphrGcwSXAmSqIN_HVXrgA9O12sTTbLYlDSoJ6qE"
+  config.application_secret = "RTidLnr0hLJifYdBSDW1MozhFuivI1UdQ5jSxHVfm4I"
+  config.application_redirect_uri = "https://localhost:3000/oauth/callback"
+  config.utm_source = "something_nice"
+end
