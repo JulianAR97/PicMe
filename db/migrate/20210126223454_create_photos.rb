4998ba39516c0eeb8e9e1baa3c3f7344ce9e6a77
Julian <julianrosenthal@yahoo.com>
2021-01-26 15:38:49 -0700

    created resources for users and photos

---
+++ db/migrate/20210126223454_create_photos.rb
@@ -0,0 +1,12 @@
+class CreatePhotos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :photos do |t|
+      t.text :thumb_url
+      t.text :small_url
+      t.text :regular_url
+      t.string :artist
+
+      t.timestamps
+    end
+  end
+end
