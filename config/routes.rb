ac4cac70d951b502275748cb84d4bb002686f40d
Julian <julianrosenthal@yahoo.com>
2021-01-26 22:34:36 -0700

    added dependencies for bootstrap and added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :digs
   resources :users
   resources :photos
+  root to: 'photos#index'
   get '/login', to: 'session#new'
   post '/login', to: 'session#create'
   delete '/logout', to: 'sessions#destroy'
